6159b9b3283c4f14469515db76e782bb2e91cd53
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-03-22 16:45:35 -0300

    env 1

--- php/src/validator-de-certificado/conexao.php
+++ php/src/validator-de-certificado/conexao.php
@@ -6,6 +6,6 @@ carregarEnv();
 try {
     $conexao = new PDO('mysql:dbname='.getenv('DB_NAME').';host='.getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'));
 } catch (PDOException $e) {
-    echo "<strong>Erro: </strong>" . $e->getMessage();
+    echo "<strong>Erro de conexão: </strong>" . $e->getMessage();
 }
 
